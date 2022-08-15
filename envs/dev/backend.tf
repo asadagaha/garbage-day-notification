57c6eb6808223cb6b00b1649a9c311dcf39c39a3
asadagaha <dongon74@gmail.com>
2022-08-15 16:32:25 +0900

    update backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -7,3 +7,24 @@ provider "aws" {
     }
   }  
 }
+
+terraform {
+  required_version = ">= 0.11.0"
+  backend "s3" {
+    bucket = "martin-terraform-tfstate-bucket"
+    region = "ap-northeast-1"
+    key = "dev/garbage_day_notification.terraform.tfstate"
+    encrypt = true
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "martin-terraform-tfstate-bucket"
+  force_destroy = true
+}
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
